072bba522aeed44ce4530890855c32e989e36beb
Aiden Gursky <gurskaideng00@isd728.org>
2022-05-17 14:48:56 +0000

    removed useless imports

--- src/impl/x86_64/commands/cmdinit.cpp
+++ src/impl/x86_64/commands/cmdinit.cpp
@@ -4,8 +4,6 @@ extern "C" void cmdinit(char input_char);
 extern "C" void print_str(char* string);
 extern "C" void print_char(char charecter);
 extern "C" void print_newline();
-extern void outb(unsigned short port, unsigned char data);
-extern unsigned char inb(unsigned short port);
 
 char input_charecter = '/';
 

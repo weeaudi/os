ad5ef0370820d7e4b2b9c21dfdf330add0f3338e
weeaudi <gurskaideng00@isd728.org>
2022-05-18 13:38:49 +0000

    potentially created the help command

--- src/impl/x86_64/commands/cmdinit.cpp
+++ src/impl/x86_64/commands/cmdinit.cpp
@@ -1,9 +1,16 @@
-#include "commands.cpp"
-
 extern "C" void cmdinit(char input_char);
 extern "C" void print_str(char* string);
 extern "C" void print_char(char charecter);
 extern "C" void print_newline();
+extern void commands_init();
+
+struct command{
+	char* name;
+    int run;
+    char *about;
+};
+
+extern command commands[1];
 
 char input_charecter = '/';
 
@@ -11,11 +18,19 @@ void cmdinit(char input_char)
 {
     input_charecter = input_char;
     print_char(input_char);
+	commands_init();
 }
+
+command current;
 
 void run_command(char *incmd){
 	print_newline();
-	commands[incmd].run()
+	for (int k = 0; k < 1; k++){
+		current = (command) commands[k];
+		if((current.name) == incmd){
+			current.run;
+		}
+	}
     print_newline();
     print_char(input_charecter);
 }
--- src/impl/x86_64/commands/commands.cpp
+++ src/impl/x86_64/commands/commands.cpp
@@ -1,16 +1,21 @@
 //this is where the commands will be defined
 
-struct command {
-    void run();
+extern "C" void print_str(char *string);
+
+struct command{
+	char* name;
+    int run;
     char *about;
 };
 
-struct command help{
-    run: void(){
-        print_str("Hello this is the W.I.P help page!");
-    },
-    about: "A W.I.P help page.",
-}
+int help_run(){print_str((char*) "Hello this is the W.I.P help page!");return 0;}
 
-struct command* commands = (struct command*) {};
-commands["help"] = help;
+struct command commands[1];
+void commands_init(){
+	command help{
+		name: (char*) "help",
+		run : help_run(),
+		about: (char*) "testing"
+	};
+	commands[0] = help;
+}

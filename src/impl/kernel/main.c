607fe30e3d52a8a3370ee0e9d6fdc9e82ca265de
weeaudi <gurskaideng00@isd728.org>
2022-05-18 13:59:39 +0000

    updated welcome message

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -7,7 +7,7 @@ void kernel_main() {
     InitilizeIDT();
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Hello welcome to aidos!\nThis operating system will be a small system that\n will be able to run windows and linux programs\n");
+    print_str("Hello welcome to aidos!\nThis operating system will be a small system that\n will be able to run windows and linux programs.\n Hope you enjoy!\n");
     cmdinit('>');
     while(1){};
 }
